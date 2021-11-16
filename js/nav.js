de80f9195cbe91d3c6ecd0b2af2da9b42ae4bedd
Huy0811 <duongchihuy.study@gmail.com>
2021-11-16 18:38:22 +0700

    fix page cart

--- js/nav.js
+++ js/nav.js
@@ -22,6 +22,7 @@ export function createNav() {
       <li class="link-item"><a href="#" class="link header-item">product</a></li>
       <li class="link-item"><a href="login.html" class="link header-item">login</a>
       <li class="link-item"><a href="product-details.html" class="link header-item">product detail</a>
+      <li class="link-item"><a href="cart.html" class="link header-item">cart</a>
     </ul>
   `;
 }
